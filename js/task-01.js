edc9766e186216b126f4113fb29a5faa5d168b66
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-01-15 17:28:15 +0200

    Adds solving the task-02

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,12 @@
 const items = document.querySelectorAll('#categories .item');
 console.log(`Number of categories: ${items.length}`);
 
-for (let item of items) {
+items.forEach(item => {
   console.log(`Category: ${item.firstElementChild.textContent}`);
   console.log(`Elements: ${item.lastElementChild.children.length}`);
-}
+})
+
+// for (let item of items) {
+//   console.log(`Category: ${item.firstElementChild.textContent}`);
+//   console.log(`Elements: ${item.lastElementChild.children.length}`);
+// }
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,36 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+
+const items = ingredients.map((ingredient) => {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent = ingredient;
+  return item;
+});
+
+list.append(...items);
+
+// const items = [];
+// ingredients.forEach((ingredient) => {
+//   const item = document.createElement('li');
+//   item.classList.add('item');
+//   item.textContent = ingredient;
+//   items.push(item);
+// });
+// list.append(...items);
+
+// ingredients.forEach((ingredient) => {
+//   const item = document.createElement('li');
+//   item.classList.add('item');
+//   item.textContent = ingredient;
+//   list.appendChild(item);
+// });
+
+// const list = document.querySelector("#ingredients");
+// const markup = ingredients
+//   .map(ingredient => `<li class="item">${ingredient}</li>`)
+//   .join("");
+// console.log(markup);
+// list.innerHTML = markup;
+// console.log(list);

dfa80b0ec7b2d53a78d0513d32d38ecef0bde248
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-01-17 16:08:24 +0200

    Adds solving the tsk-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+  const email = elements.email.value;
+  const password = elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Please fill in all the fields!");
+  };
+
+const formData = {
+  email,
+  password,
+};
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
